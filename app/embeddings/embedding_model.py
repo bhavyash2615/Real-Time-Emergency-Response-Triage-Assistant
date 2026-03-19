ac956b487dbc1c8f0e7ff42201c83af8d15507b9
Bhavya Sharma <bhavya.sh2615@gmail.com>
2026-03-19 23:23:46 +0530

    added embedding in single batch

--- app/embeddings/embedding_model.py
+++ app/embeddings/embedding_model.py
@@ -1,13 +1,22 @@
 from sentence_transformers import SentenceTransformer
 from app.config import EMBEDDING_MODEL
 
+
 class EmbeddingModel:
     def __init__(self):
         self.model = SentenceTransformer(EMBEDDING_MODEL)
 
     def embed_text(self, text: str):
-        return self.model.encode(text).tolist()
+        return self.model.encode(
+            text,
+            convert_to_numpy=True,
+            normalize_embeddings=True
+        )
 
     def embed_batch(self, texts):
-        return self.model.encode(texts).tolist()
-
+        return self.model.encode(
+            texts,
+            batch_size=32,
+            convert_to_numpy=True,
+            normalize_embeddings=True
+        )
